e5d42c46a0c69fb8a763a0060688da7db7cfc731
ahujae <ahujae@oregonstate.edu>
2018-05-01 20:45:56 -0700

    made some changes to assignment 2 part a

--- H2.hs
+++ H2.hs
@@ -1,44 +1,56 @@
 type Prog = [Cmd]
-data Cmd = Ld Int
-         | Add
-         | Mult
-         | Dup
-         
+data Cmd = LD Int
+         | ADD
+         | MULT
+         | DUP
+         deriving Show
+
 type Stack = [Int]
 type D = Stack -> Stack
 
-sem :: Prog -> D
+--sem :: Prog -> D
+sem :: [Cmd] -> Stack -> Stack
 sem [] stack = stack
+sem (x:xs) c = sem xs (semCmd x c)
 
-semCmd :: Cmd -> D
-semCmd (Ld x) stackList = (x:stackList)
+--semCmd :: Cmd -> D
+semCmd :: Cmd -> Stack -> Stack
+--semCmd (LD x) = x
 -- sem (Add) (StackList) = ((StackList !! 0) + (stackList !! 1))
 -- sem (Add) (x:stackList) = ((head stackList) + (head (tail stackList))) -- (drop 2 stackList)
 -- sem (Add) (x:stackList) = (x + (head stackList))
 -- semCmd Add (List == []) = Nothing
-semCmd (Add) ([]) = []
-semCmd (Add) (x:y:stackList) = (x+y):stackList
-semCmd (Mult) ([]) = []
-semCmd (Mult) (x:y:stackList) =  (x*y):stackList 
-semCmd (Dup) (x:stackList) = (x:x:stackList)
+semCmd (ADD) ([]) = []
+semCmd (ADD) (x:y:stackList) = (x+y):stackList
+semCmd (MULT) ([]) = []
+semCmd (MULT) (x:y:stackList) =  (x*y):stackList
+semCmd (DUP) (x:stackList) = (x:x:stackList)
+
+-- example programs
+--
+p :: Prog
+p = [LD 3,DUP,ADD,DUP,MULT]
+
+q :: Prog
+q = [LD 3,ADD]
 
 -------------------Exercise 2 part a---------------------------------------------------------
+--
+--data C = LD Int
+--       | ADD
+--       | MULT
+--       | DUP
+--       | Def String
+--       | Call String
 
-data C = LD Int
-       | ADD
-       | MULT
-       | DUP
-       | Def String
-       | Call String 
-	   
--------------------Exercise 2 part b---------------------------------------------------------      
+-------------------Exercise 2 part b---------------------------------------------------------
 
-type Macros = [(String,Prog)]
-type State = (Macros, Stack)
-type Prg = [C]
+--type Macros = [(String,Prog)]
+--type State = (Macros, Stack)
+--type Prg = [C]
 
 -------------------Exercise 2 part c---------------------------------------------------------
 
-semCmd2 :: C -> D
-semCmd2 (LD num) (x,y) = (x, (num:y))
+--semCmd2 :: C -> D
+--semCmd2 (LD num) (x,y) = (x, (num:y))
 --semCmd2 (ADD) (x,())
