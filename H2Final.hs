050cd0a989ce373ce3d09ee5392be6bf8463f644
Taylor Griffin <taylor.griffin.365@gmail.com>
2018-05-03 10:09:10 -0700

    made small changes to submission document

--- H2Final.hs
+++ H2Final.hs
@@ -1,10 +1,18 @@
-{- 
-- Names:
+{-
+
+Homework 2 (Semantics)
+CS 381, Spring 2018
+
 - Taylor Griffin
 - Blake Hudson
 - Lucien Tamno
 - Ethan Patterson
+- Ethan Ahuja
+
 -}
+
+-- Excercise 1. A Stack Language
+
 type Prog = [Cmd]
 data Cmd = LD Int
          | ADD
@@ -34,16 +42,15 @@ semCmd ADD(Just list) = if length list == 0
                         else Just (((head list) + (head (tail list))) : drop 2 list)
 
 semCmd MULT(Just list) = if length list == 0
-                       then Nothing 
+                       then Nothing
                        else if length list == 1
                        then Nothing
                        else Just (((head list) * (head (tail list))) : drop 2 list)
 
 semCmd DUP(Just list) = if length list == 0
-                       then Nothing 
+                       then Nothing
                        else Just ((list !! 0) : list)
 -- example programs
---
 p :: Prog
 p = [LD 3,DUP,ADD,DUP,MULT]
 
@@ -52,13 +59,16 @@ q = [LD 3,ADD]
 e = []
 
 empty = Just []
+
+-- testing
 test1 = sem p empty
 test2 = sem q empty
-test3 = sem e empty 
------- Part 2
---type Stack = [Int]
-type Macros = [(String,Prog2)]
-type State = (Macros, Stack)
+test3 = sem e empty
+
+-- Excercise 2. Extending the Stack Language by Macros
+
+-- a)
+
 type Prog2 = [Cmd2]
 data Cmd2 = LD2 Int
           | ADD2
@@ -68,8 +78,15 @@ data Cmd2 = LD2 Int
           | CALL String
           deriving Show
 
+-- b)
+
+type Macros = [(String,Prog2)]
+type State = (Macros, Stack)
+
 type D2 = Maybe State -> Maybe State
-          
+
+-- c)
+
 sem2 :: Prog2 -> D2
 sem2 [] stack = stack
 
@@ -81,15 +98,15 @@ semCmd2 (LD2 n) (Just(x,y)) = Just(x, (n:y))
 semCmd2 ADD2 (Just(x, list)) = if length list == 0
                                then Nothing
                                else if length list == 1
-                               then Nothing 
+                               then Nothing
                                else Just (x, (((head list) + (head (tail list))) : drop 2 list))
 
 semCmd2 MULT2 (Just(x, list)) = if length list == 0
                                then Nothing
                                else if length list == 1
-                               then Nothing 
+                               then Nothing
                                else Just (x, (((head list) * (head (tail list))) : drop 2 list))
-                               
+
 semCmd2 DUP2 (Just(x, list)) = if length list == 0
                                then Nothing
                                else Just (x, ((list !! 0) : list))
@@ -99,8 +116,8 @@ semCmd2 (DEF list stack) (Just (x,y)) = (Just(((list,stack):x), y))
 semCmd2 (CALL c) (Just(x,y)) = case lookup c x of
                                Just n -> sem2 n (Just (x,y));
                                Nothing -> (Just(x,y));
+
 -- Exercise 3. Mini Logo
--- Taylor Griffin
 
 data Cmd3 = Pen Mode
   | MoveTo Int Int
